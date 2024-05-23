a142b4030a7f4b48ac6ae7516e5c11027c78f9c1
rajesh <rajeshnagar2009@gmail.com>
2024-05-23 18:17:40 +0530

    JenkinsInstaltion Update

--- scripts/setup_jenkins.sh
+++ scripts/setup_jenkins.sh
@@ -1,10 +1,12 @@
 # scripts/setup_jenkins.sh
 #!/bin/bash
 sudo apt update
+sudo apt upgrade init-system-helpers -y
 sudo apt install -y openjdk-11-jdk
-curl -fsSL https://pkg.jenkins.io/debian/jenkins.io.key | sudo tee /usr/share/keyrings/jenkins-keyring.asc
-echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
-sudo apt update
-sudo apt install -y jenkins
-sudo systemctl start jenkins
-sudo systemctl enable jenkins
+sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
+  https://pkg.jenkins.io/debian/jenkins.io-2023.key
+echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]" \
+  https://pkg.jenkins.io/debian binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt-get update
+sudo apt-get install jenkins
