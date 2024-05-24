da258619dc12219b195a1b5df335c5fd46c8e696
rajesh <rajeshnagar2009@gmail.com>
2024-05-24 12:34:49 +0530

    jenkinssetup issue sovled

--- scripts/setup_jenkins.sh
+++ scripts/setup_jenkins.sh
@@ -1,12 +1,14 @@
 # scripts/setup_jenkins.sh
 #!/bin/bash
 sudo apt update
+sudo apt-get install init-system-helpers=1.56+nmu1~ubuntu18.04.1 -y
 sudo apt upgrade init-system-helpers -y
 sudo apt install -y openjdk-11-jdk
+sudo apt update -y
 sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
   https://pkg.jenkins.io/debian/jenkins.io-2023.key
 echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]" \
   https://pkg.jenkins.io/debian binary/ | sudo tee \
   /etc/apt/sources.list.d/jenkins.list > /dev/null
-sudo apt-get update
+sudo apt-get update -y
 sudo apt-get install jenkins
